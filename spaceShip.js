6731db5c4113aea06f57fad4644ca5aee024fa32
pablomarcel <pablomarcel@outlook.com>
2022-02-03 20:37:50 -0800

    finished spaceShip

--- spaceShip.js
+++ spaceShip.js
@@ -3,7 +3,23 @@
 // - should have a method accelerate that logs to the console 
 //   `${name} moving to ${topSpeed}`
 
+class SpaceShip{
+    constructor(name, topSpeed) {
+        this.name = name
+        this.topSpeed = topSpeed
+    }
 
+    acccelerate(){
+        const {name, topSpeed} = this
+        console.log(`${name} moving to ${topSpeed} mph`)
+    }
+}
 
 // 2. Call the constructor with a couple ships, 
 // and call accelerate on both of them.
+
+const falcon = new SpaceShip('falconOne', 1000)
+falcon.acccelerate()
+
+const bigBird = new SpaceShip('birdOne', 600)
+bigBird.acccelerate()
