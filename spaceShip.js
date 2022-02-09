27b8d93cbe6578dafbedca8b0835efd307d0ae1f
Ryan Ku <rjhk@uw.edu>
2022-02-08 19:01:14 -0800

    finished question 3

--- spaceShip.js
+++ spaceShip.js
@@ -3,7 +3,25 @@
 // - should have a method accelerate that logs to the console 
 //   `${name} moving to ${topSpeed}`
 
+class SpaceShip {
+    constructor(name, topSpeed) {
+        this.name = name; 
+        this.topSpeed = topSpeed; 
+    }
+    accelerate() {
+        console.log(`${this.name} moving to ${this.topSpeed}`)
+    };
+};
 
 
 // 2. Call the constructor with a couple ships, 
 // and call accelerate on both of them.
+
+const falcon = new SpaceShip('Falcon', 'lightspeed'); 
+falcon.accelerate();
+
+const winnebago = new SpaceShip('1986 Winnebago Chieftain 33', 'Ludicrous Speed'); 
+winnebago.accelerate();
+
+const rover = new SpaceShip('Mars Rover', 19000); 
+rover.accelerate();
