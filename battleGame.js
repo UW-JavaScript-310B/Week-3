f2dfd19d3eb11afa0770c6fe30d2ac9f28e356c9
rick <rick@rdaltonphotos.com>
2022-02-10 15:07:58 -0800

    Completed HW3

--- battleGame.js
+++ battleGame.js
@@ -1,16 +1,42 @@
 // 1. Create attack function below.  This will take the following parameters:
 // attackingPlayer, defendingPlayer, baseDamage, variableDamage
-
+function calculateVariableDamage(variableDamage)
+{
+  return Math.round(Math.random() * variableDamage)
+}
+function createAttackMessage(attackingPlayerName, defendingPlayerName, damage)
+{
+  return `${attackingPlayerName} hits ${defendingPlayerName} for ${damage} damage`
+}
+/*const attack = function(attackingPlayer, defendingPlayer, baseDamage, variableDamage)
+{
+  let damage = baseDamage + calculateVariableDamage(variableDamage)
+  defendingPlayer.health -= damage;
+  console.log(printAttackMessage(attackingPlayer, defendingPlayer, damage))
+}*/
 
 
 // 2. Create player1 and player2 objects below
 // Each should have a name property of your choosing, and health property equal to 10
+const player1 = {
+  name : 'Gandalf',
+  health : 10
+}
 
+const player2 = {
+  name : 'Saruman',
+  health : 10
+}
 
 
 // 3. Refactor attack function to an arrow function.  Comment out function above.
 
-
+const attack = (attackingPlayer, defendingPlayer, baseDamage, variableDamage) =>
+{
+  let damage = baseDamage + calculateVariableDamage(variableDamage)
+  defendingPlayer.health -= damage;
+  console.log(createAttackMessage(attackingPlayer.name, defendingPlayer.name, damage))
+}
 
 // DO NOT MODIFY THE CODE BELOW THIS LINE
 // Set attacker and defender.  Reverse roles each iteration
